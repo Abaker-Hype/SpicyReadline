c1a6056a4618cbd63b2e2a90387f93f043a9ace0
Aidan Baker <abaker@F3C027.42adel.org.au>
2022-07-12 14:50:18 +0930

    Malloc Call Optimization

--- srcs/redisplay.c
+++ srcs/redisplay.c
@@ -6,7 +6,7 @@
 /*   By: abaker <abaker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 12:04:42 by abaker            #+#    #+#             */
-/*   Updated: 2022/07/11 16:26:28 by abaker           ###   ########.fr       */
+/*   Updated: 2022/07/12 14:49:57 by abaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 #define HIDE "\e[25l"
 #define SHOW "\e[25h"
+#define CURSORSIZE 25
 
 static void	srl_cursor_start(t_srl *srl)
 {
 	char	*print;
+	char	*num;
 	int		i;
 
 	i = 1;
@@ -25,45 +27,52 @@ static void	srl_cursor_start(t_srl *srl)
 		i++;
 	if (srl->blank)
 		i--;
-	print = ft_strdup(HIDE);
+	print = ft_memcpy(ft_calloc(CURSORSIZE, sizeof(*print)), HIDE, CURSORSIZE);
 	if (i == 0)
-		print = ft_strjoinfree(print, "\r\e[0J", true, false);
+		ft_strlcat(print, "\r\e[0J", CURSORSIZE);
 	else
 	{
-		print = ft_strjoinfree(print, "\e[", true, false);
-		print = ft_strjoinfree(print, ft_itoa(i), true, true);
-		print = ft_strjoinfree(print, "F\e[0J", true, false);
+		ft_strlcat(print, "\e[", CURSORSIZE);
+		num = ft_itoa(i);
+		ft_strlcat(print, num, CURSORSIZE);
+		free(num);
+		ft_strlcat(print, "F\e[0J", CURSORSIZE);
 	}
 	ft_putstr_fd(print, srl->term.rawfd);
 	free(print);
 	srl->lastpos = srl->buffer->pos;
 }
 
-static void	srl_cursor_set(t_term *term, t_buff *buffer, int promtlen)
+static void	srl_cursor_str(char *dst, t_term *term, t_buff *buff, int plen)
+{
+	char	*num;
+	int		i;
+
+	i = (buff->chars + plen) / term->width - (buff->pos + plen) / term->width
+		- ((buff->chars + plen) % term->width == 0);
+	if (i)
+	{
+		num = ft_itoa(i);
+		ft_strlcat(dst, num, CURSORSIZE);
+		ft_strlcat(dst, "F\e[", CURSORSIZE);
+		free(num);
+	}
+	num = ft_itoa(((buff->pos + plen) % term->width) + 1);
+	ft_strlcat(dst, num, CURSORSIZE);
+	ft_strlcat(dst, "G", CURSORSIZE);
+	free(num);
+}
+
+static void	srl_cursor_set(t_term *term, t_buff *buffer, int promptlen)
 {
 	char	*print;
-	int		c;
-	int		r;
 
-	print = ft_strdup("\e[");
-	c = (buffer->pos + promtlen) % term->width;
-	r = (buffer->chars + promtlen) / term->width - (buffer->pos + promtlen)
-		/ term->width;
+	print = ft_memcpy(ft_calloc(CURSORSIZE, sizeof(*print)), "\e[", CURSORSIZE);
 	if (buffer->chars - buffer->pos)
-	{
-		if ((buffer->chars + promtlen) % term->width == 0)
-			r--;
-		if (r)
-		{
-			print = ft_strjoinfree(print, ft_itoa(r), true, true);
-			print = ft_strjoinfree(print, "F\e[", true, false);
-		}
-		print = ft_strjoinfree(print, ft_itoa(c + 1), true, true);
-		print = ft_strjoinfree(print, "G", true, false);
-	}
-	else if (c == 0)
-		print = ft_strjoinfree(print, "E", true, false);
-	print = ft_strjoinfree(print, SHOW, true, false);
+		srl_cursor_str(print, term, buffer, promptlen);
+	else if ((buffer->pos + promptlen) % term->width == 0)
+		ft_strlcat(print, "E", CURSORSIZE);
+	ft_strlcat(print, SHOW, CURSORSIZE);
 	ft_putstr_fd(print, term->rawfd);
 	free(print);
 }
